30b4cb05f5c4a66c2ec6f0a09d35f5bc98ea0a5f
nlegoff <legoff.n@gmail.com>
2013-01-07 08:51:19 +0100

    Add Client Tests

--- tests/Tiny/Tests/ClientTest.php
+++ tests/Tiny/Tests/ClientTest.php
@@ -1,7 +1,43 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+use Tiny\Client;
+use Guzzle\Plugin\Mock\MockPlugin;
+use Guzzle\Http\Message\Response;
+
+class ClientTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers Tiny\Client::__construct
+     */
+    public function testNewClient()
+    {
+        return new client();
+    }
+    
+    /**
+     * @depends testNewClient
+     * @expectedException InvalidArgumentException
+     * @covers Tiny\Client::shrink
+     */
+    public function testShrinkBadArgument($client)
+    {
+        $client->shrink('an image');
+    }
+    
+    /**
+     * @covers Tiny\Client::shrink
+     */
+    public function testShrink()
+    {
+        $returnValue = new Response('hellow');
+        $plugin = new MockPlugin(array($returnValue));
+        
+        $client = new Client();
+        $client->addSubscriber($plugin);
+        
+        $responses = $client->shrink(array(new \SplFileInfo(__FILE__)));
+        
+        $this->assertTrue(is_array($responses));
+        $this->assertEquals($returnValue, end($responses));
+    }
+}
